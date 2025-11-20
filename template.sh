b09027031b458401e6d6f198ba58f69fba5e5ef0
ritheshvreddy <rithesh.reddy22@st.niituniversity.in>
2025-11-20 05:42:06 +0530

    folder structure added

---
+++ template.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+# Creating directories     
+mkdir -p src
+mkdir -p research
+
+# Creating files
+touch src/__init__.py
+touch src/helper.py
+touch src/promt.py
+touch .env
+touch setup.py
+touch app.py
+touch research/trials.ipynb
+touch requirements.txt
+
+
+echo "Directory and files created successfully."
